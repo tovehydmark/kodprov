19c28091490b24b8aec8e3cd8b323dd9431be91d
tovehydmark <tovehydmark@gmail.com>
2023-01-16 19:16:23 +0100

    toggle theme with right symbols

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-document.getElementById('toggleTheme').addEventListener('click', () => {
+const toggleThemeBtn = document.getElementById('toggleThemeBtn');
+const themeIcon = document.getElementById('themeIcon');
+
+toggleThemeBtn.addEventListener('click', () => {
   toggletheme();
 });
 
 const toggletheme = () => {
   const body = document.body;
   body.classList.toggle('dark-mode');
+  themeIcon.classList.toggle('fa-sun');
 };

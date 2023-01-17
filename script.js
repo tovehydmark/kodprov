93bb556da7d52889dc64388095182e307913f1e7
tovehydmark <tovehydmark@gmail.com>
2023-01-17 12:48:50 +0100

    done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const toggleThemeBtn = document.getElementById('toggleThemeBtn');
 const themeIcon = document.getElementById('themeIcon');
+const heroSection = document.getElementsByClassName('hero')[0];
 
 toggleThemeBtn.addEventListener('click', () => {
   toggletheme();
@@ -9,4 +10,5 @@ const toggletheme = () => {
   const body = document.body;
   body.classList.toggle('dark-mode');
   themeIcon.classList.toggle('fa-sun');
+  heroSection.classList.toggle('hero-dark');
 };
